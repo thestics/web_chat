568f92e3385230d713475dbf7ee13703866f7ca1
Danil Kovalenko <danil.kovalenko@apostera.com>
2020-07-06 20:42:43 +0300

    added storage of chat messages

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,6 +4,9 @@
 
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+
+from chat.models import ChatMessage
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -19,7 +22,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
+        user = self.scope['user']
         message = text_data_json['message']
+        await database_sync_to_async(ChatMessage.objects.create)(text=message, author=user)
+
         to_send = {'type': 'chat_message',
                    'message': message}
         await self.channel_layer.group_send(self.room_name, to_send)
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ChatMessage(models.Model):
+
+    text = models.CharField(max_length=8192)
+    sent = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        if len(self.text) > 16:
+            text = self.text[:16] + '...'
+        else:
+            text = self.text
+        author = self.author.username
+        sent = str(self.sent)
+        return f"ChatMessage({text}, {author}, {sent})"
+
+    def __repr__(self):
+        return str(self)
